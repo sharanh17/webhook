a8d719fddd465ca0e76c79620db59eac8ac7db0a
sharan <sharanhidaduggi17@gmail.com>
2025-03-27 14:41:06 +0530

    webhhok

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json());  // Parse JSON payloads
+app.use(bodyParser.json());  
 
 // GitHub webhook endpoint
 app.post('/webhook/github', (req, res) => {
     console.log('Received webhook:', req.body);
-    res.status(200).send('Webhook added');
+    res.status(200).send('Webhook added to check ');
 });
 
 const PORT = process.env.PORT 
